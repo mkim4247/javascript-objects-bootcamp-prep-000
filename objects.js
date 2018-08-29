b8452e6029b60e11d7ba25b83ba5c1d1069143cc
Michael Kim <m.kim4247@gmail.com>
2018-08-29 22:37:18 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,1 +1,8 @@
-var playlist = { John Mayer: "Gravity"}
+var playlist = {
+  "John Mayer": "Gravity",
+  "Dave Matthews": "So Damn Lucky"
+};
+
+function updatePlaylist(playlist, artist name, song title) {
+  
+}
