a8022ac0a064546df1d2202c12581174d40a2756
Michael Kim <m.kim4247@gmail.com>
2018-08-29 22:54:19 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -7,20 +7,7 @@ function updatePlaylist(playlist, artistName, songTitle) {
   playlist[artistName] = songTitle
   return playlist
 }
-function removeFromPlaylist(playlist, artistName) {
-  delete playlist.artistName
+function removeFromPlaylist(playlist, songTitle) {
+  delete playlist.songTitle
   return playlist 
 }
-
-
-  describe('removeFromPlaylist(playlist, artistName)', () => {
-    it('removes `artistName` from `playlist`', () => {
-      removeFromPlaylist(playlist, 'Slowdive')
-
-      expect(playlist).
-        to.contain.all.keys({'My Bloody Valentine': 'Sometimes', 'Phil Ochs': "Here's to the State of Mississippi"})
-
-      expect(playlist).
-        not.to.have.all.keys({'Slowdive': 'Alison'})
-    })
-  })
